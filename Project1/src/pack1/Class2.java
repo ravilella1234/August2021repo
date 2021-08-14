f2f89936828dd4b38e4bc95ed4e191629100a6af
maha <maha@gmail.com>
2021-08-14 08:51:51 +0530

    asa

--- Project1/src/pack1/Class2.java
+++ Project1/src/pack1/Class2.java
@@ -3,11 +3,17 @@ package pack1;
 public class Class2 extends Class1
 {
 
+	
+	
 	public static void main(String[] args) 
 	{
 		Class2 c2 = new Class2();
 		c2.add();
 		c2.sub();
+		
+		System.out.println("iam 1st line");
+		System.out.println("iam 2nd line");
+		System.out.println("iam 3rd line");
 	}
 
 }
